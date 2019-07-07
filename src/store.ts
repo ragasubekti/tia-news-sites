6a468696f83f3a5316baf65387550bee0c056fd2
Raga Subekti <rfilmriot@gmail.com>
2019-07-08 01:02:24 +0700

    Remove redux tools

--- src/store.ts
+++ src/store.ts
@@ -3,11 +3,4 @@ import thunk from "redux-thunk";
 
 import RootReducer from "./modules";
 
-export const store = createStore(
-  RootReducer,
-  compose(
-    applyMiddleware(thunk),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+export const store = createStore(RootReducer, compose(applyMiddleware(thunk)));

ca9341feebb9ac0c0dcc69e9e30ebb9772dfcea5
Raga Subekti <rfilmriot@gmail.com>
2019-07-07 20:00:29 +0700

    Add Styling to Header

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -2,13 +2,34 @@ import React from "react";
 import Logo from "../assets/tia-logo.png";
 import styled from "@emotion/styled";
 
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoTextStyled = styled.div`
+  font-size: 2rem;
+  margin-left: 0.5rem;
+`;
+
+const NavbarWrapper = styled.div`
+  display: flex;
+  background: rgba(0, 0, 0, 0.05);
+  padding: 8px;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const Navbar: React.FC = () => (
-  <React.Fragment>
-    <img
-      src={Logo}
-      style={{
-        height: "50px"
-      }}
-    />
-  </React.Fragment>
+  <NavbarWrapper>
+    <LogoWrapper>
+      <img
+        src={Logo}
+        style={{
+          height: "50px"
+        }}
+      />
+      <LogoTextStyled>nanoTIA</LogoTextStyled>
+    </LogoWrapper>
+  </NavbarWrapper>
 );
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Navbar } from "./Navbar";
 
-export const Layout: React.FC = () => (
+export const Layout: React.FC = (props: React.Props<any>) => (
   <React.Fragment>
     <Navbar />
+    <div className="container">{props.children}</div>
   </React.Fragment>
 );
